75bb9c527449feb556951f13e10b4cd2bebffd06
Mateusz Mrowiec <matt.mrowiec@gmail.com>
2020-07-21 22:51:07 +0200

    Add shortcut for launching nnn

--- config.h
+++ config.h
@@ -61,6 +61,7 @@ static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufo
 static const char *termcmd[]     = { "st", NULL };
 static const char *passmenucmd[] = { "passmenu", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray3, NULL };
 static const char *screencmd[]   = { "flameshot", "gui", NULL };
+static const char *filemanagercmd[]   = { "st", "-e", "nnn", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -68,6 +69,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = passmenucmd } },
 	{ MODKEY,                       XK_s,      spawn,          {.v = screencmd } },
+	{ MODKEY,                       XK_n,      spawn,          {.v = filemanagercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
