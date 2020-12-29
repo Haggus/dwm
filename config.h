3a8dffa9eb370acfe8c083d246a9861d0048a1eb
Mateusz Mrowiec <matt.mrowiec@gmail.com>
2020-12-29 12:36:27 +0100

    Update theme

--- config.h
+++ config.h
@@ -7,13 +7,13 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Mono,Fira Mono Medium:style=Medium,Regular:size=9" };
-static const char dmenufont[]       = "Fira Mono,Fira Mono Medium:style=Medium,Regular:size=9";
-static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#3c3836";
-static const char col_gray3[]       = "#ebdbb2";
-static const char col_gray4[]       = "#928374";
-static const char col_green[]       = "#98971a";
+static const char *fonts[]          = { "Fira Mono,Fira Mono Medium:style=Medium,Regular:size=11" };
+static const char dmenufont[]       = "Fira Mono,Fira Mono Medium:style=Medium,Regular:size=11";
+static const char col_gray1[]       = "#1d1f21";
+static const char col_gray2[]       = "#282a2e";
+static const char col_gray3[]       = "#c5c8c6";
+static const char col_gray4[]       = "#969896";
+static const char col_green[]       = "#b5bd68";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -65,11 +65,11 @@ static const char *mutemic[] = { "/usr/bin/pactl", "set-source-mute",       "1",
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray3, NULL };
-static const char *termcmd[]        = { "st", NULL };
-static const char *passmenucmd[]    = { "passmenu", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray3, NULL };
+static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray1, NULL };
+static const char *termcmd[]        = { "alacritty", NULL };
+static const char *passmenucmd[]    = { "passmenu", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray1, NULL };
 static const char *screencmd[]      = { "flameshot", "gui", NULL };
-static const char *filemanagercmd[] = { "st", "-e", "nnn", NULL };
+static const char *filemanagercmd[] = { "alacritty", "-e", "nnn", NULL };
 static const char *screenlockcmd[]  = { "slock", NULL };
 
 static Key keys[] = {
